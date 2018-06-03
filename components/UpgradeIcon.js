fcebbaead5ddf600f340d198d4aa75519e980af7
Evan Bacon <baconbrix@gmail.com>
2018-06-02 21:19:09 -0700

    Updated animation

--- components/UpgradeIcon.js
+++ components/UpgradeIcon.js
@@ -2,8 +2,19 @@ import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
 import { Image } from 'react-native';
 import Assets from '../Assets';
+
+import * as Animatable from 'react-native-animatable';
+
 export default (UpgradeIcon = props => (
-  <Image {...props} source={Assets.images.arrow['green.png']} />
+  <Animatable.Image
+    iterationCount="infinite"
+    animation={'jello'}
+    duration={2000}
+    iterationDelay={Math.random() * 1000}
+    useNativeDriver={false}
+    {...props}
+    source={Assets.images.arrow['green.png']}
+  />
 ));
 
 // <Ionicons size={32} color="#ACFA76" {...props} name="md-star" />
